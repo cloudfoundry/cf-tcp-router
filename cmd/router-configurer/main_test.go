305ad20c72f15bb33d6e1bb7053d601628927f5b
Edwin Xie <exie@pivotal.io>
2016-09-06 16:31:15 -0700

    Added retry logic to routing_api connection in unit test

--- cmd/router-configurer/main_test.go
+++ cmd/router-configurer/main_test.go
@@ -80,8 +80,12 @@ var _ = Describe("Main", func() {
 
 	getRouterGroupGuid := func(port uint16) string {
 		client := routing_api.NewClient(fmt.Sprintf("http://127.0.0.1:%d", port), false)
-		routerGroups, err := client.RouterGroups()
-		Expect(err).NotTo(HaveOccurred())
+		var routerGroups []models.RouterGroup
+		Eventually(func() error {
+			var err error
+			routerGroups, err = client.RouterGroups()
+			return err
+		}, "30s", "1s").ShouldNot(HaveOccurred(), "Failed to connect to Routing API server after 30s.")
 		Expect(routerGroups).ToNot(HaveLen(0))
 		return routerGroups[0].Guid
 	}
